1906540c5c648e0fb41193eaf3b4912e93694c45
Leon Berenschot <leon@kerkdienstgemist.nl>
2024-09-10 12:37:21 +0200

    🩹 fix: MiniTest::Unit::TestCase is now Minitest::Test

--- test/test_to_json.rb
+++ test/test_to_json.rb
@@ -1,6 +1,6 @@
 require 'minitest_helper'
 
-class TestToJson < MiniTest::Unit::TestCase
+class TestToJson < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::ToJson::VERSION
   end
